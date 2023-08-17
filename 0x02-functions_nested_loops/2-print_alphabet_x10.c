53c179c9b16be4dd03caebcfac6a93c17eabd2e8
Hangolden <chikaiheme80@gmail.com>
2023-08-16 19:43:36 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,16 @@
  * print_alphabet_x10 - prints alphabet in lowercase 10x
  * Return: 0
  */
-void print_alphabet_x10(void);
+
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 'a' ; j <= 'z' ; j++)
-			_putchar(j);
+		for (j = 16 ; j < 23 ; j++)
+		       _putchar(j);
 		_putchar('\n');
 	}
 }
