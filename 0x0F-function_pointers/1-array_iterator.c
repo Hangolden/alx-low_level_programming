45938f5ff50df6897bca318dfe9d26c62a693145
Hangolden <chikaiheme80@gmail.com>
2023-09-14 17:06:33 -0700

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
-include "function_pointers.h
+#include "function_pointers.h"
 #include <stdio.h>
 /**
-* array_iterator - prints each array element on a new line
+* array_iterator - prints each array elem on a newl
 * @array: array
 * @size: how many elem to print
 * @action: pointer to print in regular or hex
